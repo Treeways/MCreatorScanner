547a1a85d0c10e9a21e9c78bb5794d03a0e7a627
Treeway7 <56928485+Treeway7@users.noreply.github.com>
2023-04-13 20:12:13 -0500

    Initial commit!

---
+++ main.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+from tkinter import filedialog
+from scanner import scanner
+
+class MCreatorScanner():
+
+    def __init__(self) -> None:
+        self.padding = 20
+
+        self.root = tk.Tk()
+        self.root.title('MCreator Mod Scanner')
+        self.root.geometry('400x200')
+
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
+
+        self.modpath = tk.StringVar()
+        self.modpath.set('No path selected')
+
+        self.frame = tk.Frame(self.root)
+        self.frame.grid(row=0, column=0)
+        self.frame.grid_rowconfigure(0, weight=1)
+        self.frame.grid_rowconfigure(1, weight=1)
+
+        self.button = tk.Button(self.root, text="Select mods/ folder", command=self.get_modpath)
+        self.button.grid(row=0, column=0, pady=self.padding)
+
+        self.label = tk.Label(self.root, text=self.modpath.get())
+        self.label.grid(row=1, column=0, pady=self.padding)
+
+        self.root.mainloop()
+
+    def get_modpath(self):
+        directory = filedialog.askdirectory()
+        if not directory:
+            return
+        self.modpath.set(directory)
+        self.label.config(text=f"Scanning mod folder:\n{self.modpath.get()}")
+        self.label.config(text=scanner(self.modpath.get()))
+
+MCreatorScanner()
---
+++ scanner.py
@@ -0,0 +1,27 @@
+import os
+import zipfile
+import sys
+
+def scanner(input_path):
+    path = input_path + '/'
+    mods = [name for name in os.listdir(path) if name.endswith('.jar') or name.endswith('.jar.disabled')]
+
+    mcreator_mods = []
+    for mod in mods:
+        is_mcreator = False
+        zfile = zipfile.ZipFile(path + mod, 'r')
+        try:
+            infolist = zfile.infolist()
+            for zipinfo in infolist:
+                filename = zipinfo.filename
+                if "mcreator" in filename and not is_mcreator:
+                    is_mcreator = True
+                    mcreator_mods.append(mod)
+        except:
+            pass
+
+    if (not mcreator_mods):
+        return "No MCreator mods were found!"
+
+    newline = '\n'
+    return f"MCreator mods were found!\n{newline.join(mcreator_mods)}"
